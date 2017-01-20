2a38378f6f74bf2958cc97795e4adf9661c4f8d6
Asad Hasan <asad.hasan@infinityworks.com>
2017-01-20 15:06:32 +0000

    Commiting skeleton

---
+++ src/test/java/features/stepdefinitions/SearchAutoCompletion.java
@@ -0,0 +1,31 @@
+package features.stepdefinitions;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import PageObjects.Homepage;
+
+public class SearchAutoCompletion {
+	
+	@Drone private WebDriver driver;
+	@Page private Homepage Homepage;
+	
+	@Given("^I am on the homepage$")
+	public void I_am_on_the_homepage(){
+	    
+	}
+
+	@When("^I type laptop$")
+	public void I_type_text(){
+
+	}
+
+	@Then("^laptop computer should appear in autocomplete suggestions$")
+	public void result_should_appear_in_suggestions(){
+  
+	}
+}
